3816164d978316784b14d507a1688982fbbbf7a3
mangukianisarg <mangukianisarg@gmail.com>
2023-10-11 09:36:20 +0530

    Initial commit

---
+++ lib/pages/feeds.dart
@@ -0,0 +1,112 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:social_app/models/post.dart';
+import 'package:social_app/utils/constants.dart';
+import 'package:social_app/utils/firebase.dart';
+import 'package:social_app/widgets/indicators.dart';
+import 'package:social_app/widgets/userpost.dart';
+
+class Feeds extends StatefulWidget {
+  @override
+  _FeedsState createState() => _FeedsState();
+}
+
+class _FeedsState extends State<Feeds> with AutomaticKeepAliveClientMixin{
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int page = 5;
+  bool loadingMore = false;
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        setState(() {
+          page = page + 5;
+          loadingMore = true;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('>>>');
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          Constants.appName,
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        color: Theme.of(context).colorScheme.secondary,
+        onRefresh: () =>
+            postRef.orderBy('timestamp', descending: true).limit(page).get(),
+        child: SingleChildScrollView(
+          // controller: scrollController,
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: FutureBuilder(
+                  future: postRef
+                      .orderBy('timestamp', descending: true)
+                      .limit(page)
+                      .get(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      var snap = snapshot.data;
+                      List docs = snap!.docs;
+                      return ListView.builder(
+                        controller: scrollController,
+                        itemCount: docs.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          PostModel posts =
+                              PostModel.fromJson(docs[index].data());
+                          return Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: UserPost(post: posts),
+                          );
+                        },
+                      );
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return circularProgress(context);
+                    } else
+                      return Center(
+                        child: Text(
+                          'No Feeds',
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
